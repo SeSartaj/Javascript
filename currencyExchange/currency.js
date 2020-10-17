17e78e93d3e445355cd69d5ef3e0231c4e72c899
= <sartajahmad1529@gmail.com>
2020-10-17 07:32:00 +0500

    currencyExchangev2.0 added

--- currencyExchange/currency.js
+++ currencyExchange/currency.js
@@ -1,14 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('form').onsubmit = function () {
-        // var from = document.querySelector('#from').value.toUpperCase(); 
-        fetch('https://api.exchangeratesapi.io/latest?base=USD')
+        var from = document.querySelector('#from').value.toUpperCase(); 
+        var to = document.querySelector('#to').value.toUpperCase();
+        fetch(`https://api.exchangeratesapi.io/latest?base=${from}`)
         .then(response => response.json())
         .then(data => {
-            const currency = document.querySelector('#currency').value.toUpperCase();
+            const currency = to;
             const rate = data.rates[currency];
 
             if(rate !== undefined) {
-                document.querySelector('#result').innerHTML = `One USD is equal to ${rate} ${currency}`;
+                document.querySelector('#result').innerHTML = `One ${from} is equal to ${rate} ${currency}`;
             }
             else {
                 document.querySelector('#result').innerHTML = `Invalid Currency`;
